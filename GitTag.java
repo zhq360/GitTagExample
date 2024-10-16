e15e7cb557ca8c05cb3eb7ff96f315d14262b1f6
zhq <3239529825@qq.com>
2024-10-16 23:54:48 +0800

    Added output: 我是张翰权二号

--- GitTag.java
+++ GitTag.java
@@ -4,7 +4,8 @@ public class GitTag {
     public static void main(String[] args) {
         System.out.println("Version1.0");
         System.out.println("我是张翰权");
-    }
+
+ System.out.println("张翰权二号")    }
 }
 
 
